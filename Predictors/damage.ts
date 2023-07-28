53570be7efdede8afa74977df0f4752a7e63355a
0xffabc <139798156+0xffabc@users.noreply.github.com>
2023-07-28 19:04:19 +0500

    qwerqw

--- Predictors/damage.ts
+++ Predictors/damage.ts
@@ -1,1 +1,15 @@
+/* Import script */
 
+import { NewtonIteration } from "./NewtonIteration";
+
+/* Damage prediction */
+
+function wave_function(x: number): number {
+    const damages = [
+                        [68, 50, 25],
+                        [46, 50, 25], 
+                        [80, 50, 25], 
+                        [25, 80]
+                    ];
+    damages
+};
